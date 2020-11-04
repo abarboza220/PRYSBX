9b3b478380c64de22d879912473b8e88bf16f9d1
Alberto Barboza <abarboza220@gmail.com>
2020-11-04 13:00:40 -0600

    Addicione mas lineas a las pruebas

--- 2_variables.sh
+++ 2_variables.sh
@@ -1,7 +1,7 @@
 # !/bin/bash
 # Programa para revisar la declaracion de variables
-
-
+#I am here for you my Good
+#I can do anything if you are with me , Good
 opcion=0
 nombre="Alberto"
 apellido="Barboza"
@@ -10,4 +10,5 @@ echo "Opcion : $opcion Nombre : $nombre Apelido : $apellido"
 # Exportar la varibale para que este disponible a los demas procesos
 export nombre
 #Exportar variable
+#Todo lo puedo en Cristo que me fortalece
 #./2_variables_2.sh
