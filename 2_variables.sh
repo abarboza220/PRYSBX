1efbdfbd26afb5038a9bd2f1190b5205f66e7faa
Alberto Barboza <abarboza220@gmail.com>
2020-11-10 17:27:13 -0600

    Adios comflicto

--- 2_variables.sh
+++ 2_variables.sh
@@ -9,11 +9,10 @@ estadoCivil="Casado con hijos"
 Expertis="NodoJS,Scrum"
 testGit="QAEntorno"
 cambio="arch_20201107_V2356_"
-<<<<<<< HEAD
 strhtml="<html><head>Pruebas varias</head></html>"
-=======
+
+
 Lema="Todo lo puedo en Cristo que me fortalece,La sangre de Cristo tiene poder"
->>>>>>> 5f65573490a7e1e928d3fbba1f965240df4957da
 echo "Opcion : $opcion Nombre : $nombre Apelido : $apellido"
 # Exportar la varibale para que este disponible a los demas procesos
 export nombre
