76ff56e4c7b3a67a6cb60fbb5f2b09c4e1887835
Colaborador empoderado <estudiocamilabb@gmail.com>
2020-11-10 16:50:48 -0600

    Primer Commit

--- 2_variables.sh
+++ 2_variables.sh
@@ -9,6 +9,7 @@ estadoCivil="Casado con hijos"
 Expertis="NodoJS,Scrum"
 testGit="QAEntorno"
 cambio="arch_20201107_V2356_"
+strhtml="<html><head>Pruebas varias</head></html>"
 echo "Opcion : $opcion Nombre : $nombre Apelido : $apellido"
 # Exportar la varibale para que este disponible a los demas procesos
 export nombre
